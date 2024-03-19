c21f085134214d9b3ae7e179b7a28df33b6938bf
darlon <darlon.carvalho@hotmail.com>
2024-03-19 20:39:39 -0300

    Estutando Spring

---
+++ RestApiSpring/src/test/java/org/darlonsantos/restapispring/RestApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.darlonsantos.restapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

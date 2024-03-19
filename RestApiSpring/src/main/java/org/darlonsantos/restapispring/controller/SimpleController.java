eb39e8a3f3ea409c734d2f47d1ebbd72de38bc12
darlon <darlon.carvalho@hotmail.com>
2024-03-19 20:59:58 -0300

    Estutando Spring

---
+++ RestApiSpring/src/main/java/org/darlonsantos/restapispring/controller/SimpleController.java
@@ -0,0 +1,20 @@
+package org.darlonsantos.restapispring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+  public static String OLA_MUNDO =  "Olá Mundo!";
+
+
+
+  @GetMapping("/hello")
+  public String hello(){
+   return OLA_MUNDO;
+
+  }
+
+
+}
